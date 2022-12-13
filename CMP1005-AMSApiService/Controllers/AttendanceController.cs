5fc5e1f0448c54127ead316eb203363b36e4d3be
Rekell Caraan <caraanr@mcgi.org>
2022-12-13 03:13:49 -0500

    Updated/Cleanup Solution

--- CMP1005-AMSApiService/Controllers/AttendanceController.cs
+++ CMP1005-AMSApiService/Controllers/AttendanceController.cs
@@ -7,55 +7,54 @@ using AMSLibrary.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace CMP1005_AMSApiService.Controllers
+namespace CMP1005_AMSApiService.Controllers;
+
+[Route("[controller]")]
+public class AttendanceController : ControllerBase
 {
-    [Route("[controller]")]
-    public class AttendanceController : ControllerBase
+    private readonly IAttendanceDBContext attendanceDBContext = new AttendanceDBContext();
+
+    public AttendanceController()
     {
-        private readonly IAttendanceDBContext attendanceDBContext = new AttendanceDBContext();
 
-        public AttendanceController()
-        {
+    }
 
-        }
+    public AttendanceController(IAttendanceDBContext dbContext)
+    {
+        attendanceDBContext = dbContext;
+    }
 
-        public AttendanceController(IAttendanceDBContext dbContext)
-        {
-            attendanceDBContext = dbContext;
-        }
+    // GET: api/values
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+        return new string[] { "value1", "value2" };
+    }
 
-        // GET: api/values
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
+    // GET api/values/5
+    [HttpGet("{id}")]
+    public string Get(int id)
+    {
+        return "value";
+    }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
+    // POST api/values
+    [HttpPost]
+    public void Post([FromBody]Attendance attendance)
+    {
+    }
 
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]Attendance attendance)
-        {
-        }
+    // PUT api/values/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody]Attendance attendance)
+    {
+        attendanceDBContext.MarkAsModified(attendance);
+    }
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]Attendance attendance)
-        {
-            attendanceDBContext.MarkAsModified(attendance);
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+    // DELETE api/values/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
     }
 }
 
