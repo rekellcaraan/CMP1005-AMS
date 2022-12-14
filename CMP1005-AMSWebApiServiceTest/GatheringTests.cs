a93dcd747462a2166181c2deecdf5a1389f472dd
Rekell Caraan <caraanr@mcgi.org>
2022-12-13 23:52:33 -0500

    Started adding UnitTest for GatheringController

---
+++ CMP1005-AMSWebApiServiceTest/GatheringTests.cs
@@ -0,0 +1,21 @@
+﻿namespace CMP1005_AMSWebApiServiceTest;
+using CMP1005_AMSApiService.Controllers;
+using CMP1005_AMSApiService.Data;
+using Microsoft.EntityFrameworkCore;
+using AMSLibrary.Models;
+
+[TestClass]
+public class GatheringTests
+{
+    [TestMethod]
+    public void WhenGettingAllAvailableGatherings_ThenListAllCreatedAvailableGatherings()
+    {
+        using(var amssqllite = new AMSSQLLite()) {
+
+            var controller = new GatheringController(amssqllite.Context);
+            var gatheringsList = controller.GetAllAvailableGatherings();
+
+        }
+    }
+}
+
---
+++ CMP1005-AMSWebApiServiceTest/SQLLiteTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+using AMSLibrary.Models;
+using CMP1005_AMSApiService.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CMP1005_AMSWebApiServiceTest;
+public class AMSSQLLite : IDisposable
+{
+    private const string InMemoryConnectionString = "DataSource=:memory:";
+    private readonly SqliteConnection _connection;
+
+    public AMSDBContext Context { get; set; }
+
+    public AMSSQLLite()
+    {
+        _connection = new SqliteConnection(InMemoryConnectionString);
+        _connection.Open();
+        var options = new DbContextOptionsBuilder<AMSDBContext>()
+        .UseSqlite(_connection)
+        .Options;
+        Context = new AMSDBContext(options);
+        Context.Database.EnsureCreated();
+    }
+
+    public void Dispose()
+    {
+        _connection.Close();
+    }
+}
+
