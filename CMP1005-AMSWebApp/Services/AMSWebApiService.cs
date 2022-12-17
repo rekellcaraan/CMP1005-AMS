98ef89ae56af156fc65ccbd312e7fc551f61684e
Rekell Caraan <caraanr@mcgi.org>
2022-12-17 12:23:35 -0500

    Added style; added AMSWebApiService class

---
+++ CMP1005-AMSWebApp/Services/AMSWebApiService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace CMP1005_AMSWebApp.Services;
+
+public class AMSWebApiService
+{
+	private Uri amsBaseUrl = new Uri("https://localhost:7136/ams");
+	static readonly HttpClient amsHttpClient = new HttpClient();
+
+	public AMSWebApiService()
+	{
+	}
+}
+
