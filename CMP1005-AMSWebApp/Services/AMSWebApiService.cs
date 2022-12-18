6fc6cb0616a99dc68b50da1a25cb9b7d8578853a
Rekell Caraan <caraanr@mcgi.org>
2022-12-17 23:07:44 -0500

    Updated AMSWebApiService with Post action

--- CMP1005-AMSWebApp/Services/AMSWebApiService.cs
+++ CMP1005-AMSWebApp/Services/AMSWebApiService.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using CMP1005_AMSWebApp.Models;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Text.Json;
+using System.Text;
 
 namespace CMP1005_AMSWebApp.Services;
 
@@ -15,22 +18,18 @@ public class AMSWebApiService
 
 	public async Task<List<GatheringModel>> GetAvailableGatherings()
 	{
-
         List<GatheringModel> availableGatherings = new List<GatheringModel>();
 
         try
         {
-
             using (HttpResponseMessage response = await amsHttpClient.GetAsync(String.Join('/', amsBaseUrlString, "gathering")))
             {
                 response.EnsureSuccessStatusCode();
                 string? responseBody = await response.Content.ReadAsStringAsync()!;
 
                 if (!String.IsNullOrEmpty(responseBody))
-                { availableGatherings = JsonConvert.DeserializeObject<List<GatheringModel>>(responseBody); }
-
+                { availableGatherings = JsonConvert.DeserializeObject<List<GatheringModel>>(responseBody) ?? new List<GatheringModel>(); }
             }
-
         }
         catch (HttpRequestException e)
         {
@@ -40,5 +39,22 @@ public class AMSWebApiService
 
         return availableGatherings!;
     }
+
+    public async void CreateNewGathering(GatheringModel newGathering)
+    {
+        try
+        {
+            var newGatheringJsonSerialized = System.Text.Json.JsonSerializer.Serialize(newGathering);
+            var requestContent = new StringContent(newGatheringJsonSerialized, Encoding.UTF8, "application/json");
+
+            var response = await amsHttpClient.PostAsync(String.Join('/', amsBaseUrlString, "gathering"), requestContent);
+        }
+        catch (HttpRequestException e)
+        {
+            Console.WriteLine("\nException Caught!");
+            Console.WriteLine("Message :{0} ", e.Message);
+        }
+    }
+
 }
 
