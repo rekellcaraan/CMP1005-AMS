a99892176453108daf1ed201cc8a5abab3afa68b
Rekell Caraan <caraanr@mcgi.org>
2022-12-17 23:32:18 -0500

    Updated NewGatheringModel Page for Post action calling the AMSWebApiService

--- CMP1005-AMSWebApp/Pages/NewGatheringModel.cshtml.cs
+++ CMP1005-AMSWebApp/Pages/NewGatheringModel.cshtml.cs
@@ -6,22 +6,25 @@ using AMSLibrary.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using CMP1005_AMSWebApp.Models;
+using CMP1005_AMSWebApp.Services;
 
 namespace CMP1005_AMSWebApp.Pages
 {
     public class NewGatheringModelModel : PageModel
     {
-        [BindProperty]
-        public GatheringModel GatheringEvent { get; set; } = default!;
+        private readonly AMSWebApiService _amswebapiServce;
 
-        public void OnGet()
+        public NewGatheringModelModel(AMSWebApiService amswebapiService)
         {
-
+            _amswebapiServce = amswebapiService;
         }
+
+        [BindProperty]
+        public GatheringModel newGathering { get; set; } = default!;
 
         public void OnPost()
         {
-
+            _amswebapiServce.CreateNewGathering(newGathering);
         }
 
     }
